2857009dc773d62a646cedeac4bbe581a6afac38
Cristian Cienfuegos <cristian100@gmail.com>
2013-12-17 18:26:41 -0600

    changes

--- themes/qft/templates/system/page.tpl.php
+++ themes/qft/templates/system/page.tpl.php
@@ -97,29 +97,35 @@
       <?php print render($page['navigation']); ?>
     </div>
   </header>
+  
+  <?php print render($page['highlighted']); ?>
 
   <div class="l-main">
-    <div class="l-content" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+    <div class="l-two-columns">
+      <div class="l-col-left">
+        <div class="l-content" role="main">
+          <div class="l-content-inner">
+            <?php print $breadcrumb; ?>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+              <h1><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php print render($tabs); ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+              <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php print render($page['content']); ?>
+            <?php print $feed_icons; ?>
+          </div>
+        </div>
+        <?php print render($page['sidebar_second']); ?>
+      </div>
     </div>
-
-    <?php print render($page['sidebar_first']); ?>
-    <?php print render($page['sidebar_second']); ?>
+    
   </div>
 
   <footer class="l-footer" role="contentinfo">
