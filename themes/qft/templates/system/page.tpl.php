9aa116cc1ad50cbb90bb0b155cccc00342e77401
Cristian Cienfuegos <cristian100@gmail.com>
2014-01-28 14:04:46 -0700

    changes for theme

--- themes/qft/templates/system/page.tpl.php
+++ themes/qft/templates/system/page.tpl.php
@@ -107,13 +107,13 @@
       <div class="l-col-left">
         <div class="l-content" role="main">
           <div class="l-content-inner">
-            <?php print $breadcrumb; ?>
             <a id="main-content"></a>
             <?php print render($title_prefix); ?>
             <?php if ($title): ?>
               <h1><?php print $title; ?></h1>
             <?php endif; ?>
             <?php print render($title_suffix); ?>
+            <?php print $breadcrumb; ?>
             <?php print $messages; ?>
             <?php print render($tabs); ?>
             <?php print render($page['help']); ?>
